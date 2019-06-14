fc0bdf1105426e7ef9e75d47d7be2a86411d0431
Jinpeng Wang <haataa@qq.com>
2019-06-15 00:25:06 +0800

    docs: add docstring in train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -11,6 +11,7 @@ import pickle
 
 import nltk
 nltk.download(['punkt', 'wordnet','stopwords'])
+from nltk import WordNetLemmatizer
 
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -21,6 +22,16 @@ from sklearn.metrics import classification_report,accuracy_score
 
 
 def load_data(database_filepath):
+    """Load and return the needed data
+    
+    Args:
+    database_filename: string. SQLite database filepath.
+       
+    Returns:
+    X: dataframe. Features dataset.
+    Y: dataframe. Labels dataset.
+    category_names: list of strings. List containing category names.
+    """
     engine = create_engine("sqlite:///{}".format(database_filepath))
     df = pd.read_sql_table("messages", con=engine)
     #df.head()
@@ -32,18 +43,31 @@ def load_data(database_filepath):
     return X,Y,category_names
 
 def tokenize(text):
+    """Normalize, tokenize and lemmatize text string
+    
+    Args:
+    text: string. String to be cleaned
+       
+    Returns:
+    words: list of strings. List containing clean word tokens
+    """
     # step 1 clean text, remove punctuation and turn into lower cases
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower()) 
     # tokenize 
     words = word_tokenize(text)
     # remove stop words
     words = [w for w in words if w not in stopwords.words("english")]
-    # Stem word tokens
-    stemmed = [PorterStemmer().stem(w) for w in words]
-    return stemmed
+    # lemmatize word tokens
+    words = [WordNetLemmatizer().lemmatize(w).lower().strip() for w in words]
+    
+    return words
 
 
 def build_model():
+    """Build a machine learning pipeline.
+    Return:
+        cv : GridSearch model
+    """
     pipeline = Pipeline([
     ('vect', CountVectorizer(tokenizer = tokenize)),
     ('tfidf', TfidfTransformer()),
@@ -54,18 +78,20 @@ def build_model():
               'tfidf__use_idf':[True, False],
               'clf__estimator__n_estimators':[10, 20]}
 
-    model = GridSearchCV(pipeline, param_grid = parameters,verbose = 3)
-    return model
+    cv = GridSearchCV(pipeline, param_grid = parameters,verbose = 3)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
     """
     Shows model's performance on test data
-    Args:
-    model: trained model
-    X_test: Test features
-    Y_test: Test targets
-    category_names: Target labels
+    Args :
+        model: trained model
+        X_test: Test features
+        Y_test: Test targets
+        category_names: Target labels
+    Return:
+        None
     """
 
     # predict
@@ -79,6 +105,13 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    """Save the trained model in a pickle file
+    Args :
+        model : trained model.
+        model_filepath : string. Filepath of the model to be saved
+    Returns:
+        None
+    """
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
